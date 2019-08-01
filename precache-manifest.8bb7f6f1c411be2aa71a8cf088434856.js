9f1f0af7289f148063dd53a75d71c5a46a641f61
Mason Gil <35542652+MasonEG@users.noreply.github.com>
2019-07-31 19:14:40 -0500

    Fixed the broken build lol
    images weren't showing because I didn't know about the homepage property, I did switch to CRA from nano-react-app but there is no difference

---
+++ precache-manifest.8bb7f6f1c411be2aa71a8cf088434856.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1b49715e5e8d51cd96e67c6cc73c1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "724a84db502cfe023adf",
+    "url": "./static/css/main.4d023a49.chunk.css"
+  },
+  {
+    "revision": "cfa33ebb859bbe5592b6",
+    "url": "./static/js/2.9a53d978.chunk.js"
+  },
+  {
+    "revision": "724a84db502cfe023adf",
+    "url": "./static/js/main.90d1e554.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6c18c12cfbe24c948cd13392483d5f98",
+    "url": "./static/media/ChatClient.6c18c12c.PNG"
+  },
+  {
+    "revision": "a975c1db10caf756d6112946884c43e1",
+    "url": "./static/media/InitialWebsite.a975c1db.PNG"
+  },
+  {
+    "revision": "6246769b91e1ed197fc59c043c8ae135",
+    "url": "./static/media/MyPic.6246769b.jpg"
+  },
+  {
+    "revision": "ff1d322daa20d811d983e49ee7a0a4a3",
+    "url": "./static/media/githublogo.ff1d322d.png"
+  }
+]);
